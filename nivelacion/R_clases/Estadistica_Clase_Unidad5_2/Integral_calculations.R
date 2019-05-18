74d77f3b39f2241adab22b79745e6b69c6ac4457
Juan Rocha Amado <jp.rocha@icloud.com>
2019-05-18 01:45:51 -0300

    Cambios por TP Nro 6

---
+++ nivelacion/R_clases/Estadistica_Clase_Unidad5_2/Integral_calculations.R
@@ -0,0 +1,43 @@
+# http://www.rpubs.com/venkatritch/333327
+f=expression(x^2+3*x)
+D(f,'x')
+D(D(f,'x'),'x')
+
+f=expression(-n*x)
+D(f,'x')
+D(D(f,'x'),'x')
+
+
+# http://homepages.math.uic.edu/~jyang06/stat522/handouts/handout8.pdf
+## define the integrated function
+integrand <- function(x) {1/((x+1)*sqrt(x))}
+## integrate the function from 0 to infinity
+integrate(integrand, lower = 0, upper = Inf)
+
+
+#https://cran.r-project.org/web/packages/PolynomF/PolynomF.pdf
+#https://www.rdocumentation.org/packages/PolynomF/versions/1.0-2
+f<-function(x) 2*x
+integrate(f,0,2)
+
+f=expression(2*x)
+integral(2*x, limits = NULL)
+
+
+
+# NOT RUN {
+p <- poly.from.zeros(-2:5)
+ip <- integral(p)
+ipv <- integral(p, limits = c(-2, 5))
+
+plot(polylist(p, deriv(p)))
+
+x <- polynom()
+H <- polylist(1, x)Y
+for(n in 2:10)
+  H[[n+1]] <- x * H[[n]] - (n-1)*H[[n-1]]
+
+solve(deriv(H))
+
+# }
+library(PolynomF)
